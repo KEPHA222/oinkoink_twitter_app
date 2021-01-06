41d7ea58be0dff81e7c32a163f67b6725af40073
kepha222 <kephamotari1@gmail.com>
2021-01-06 12:03:40 +0300

    second commit

--- oinkoink/settings.py
+++ oinkoink/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bf5xc8_14qvn@vxnib@t1^13ngwybz_cl)ir5(z-m2gz%*_5)0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
